38a21e8fd09c40560f2d12019791cc73651b1128
TaiHoang07 <hoangductai2007@gmail.com>
2021-12-14 16:06:27 +0700

    Fix filter data code

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -11,27 +11,25 @@ const Container = ({
   setShowModal,
   setSelectedData,
 }) => {
-  const newData = [...data].filter((item) => item.type === type);
+  const filterData = [...data]
+    .filter((data) => data.type === type)
+    .filter((data) => status === "all" || data.status === status)
+    .filter(
+      (data) =>
+        searchInput === "" || data.name.toLowerCase().includes(searchInput)
+    );
 
   return (
     <div className="container">
-      {newData.lenght !== 0 &&
-        newData
-          .filter((data) => status === "all" || data.status === status)
-          .filter(
-            (data) =>
-              searchInput === "" ||
-              data.name.toLowerCase().includes(searchInput)
-          )
-          .map((data) => (
-            <Card
-              key={data._id}
-              data={data}
-              setModalType={setModalType}
-              setShowModal={setShowModal}
-              setSelectedData={setSelectedData}
-            />
-          ))}
+      {filterData.map((data) => (
+        <Card
+          key={data._id}
+          data={data}
+          setModalType={setModalType}
+          setShowModal={setShowModal}
+          setSelectedData={setSelectedData}
+        />
+      ))}
     </div>
   );
 };
